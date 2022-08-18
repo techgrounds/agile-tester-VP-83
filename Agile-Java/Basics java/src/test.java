3a0c33c5bebd40acac7dae0faf9ff3b25716f8b7
VP-83 <111268624+VP-83@users.noreply.github.com>
2022-08-18 14:17:06 +0200

    Java: ik snap er nog niets van

--- Agile-Java/Basics java/src/test.java
+++ Agile-Java/Basics java/src/test.java
@@ -1,5 +1,67 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World dit is Viviane");
-    }
+
+
+            //public = Iedereen kan het zien
+            //private = Strict tot dit bestand
+
+//        String nummer = "Ik ben";
+//        String nummer2 = "Mitch";
+//
+//        String uitkomst = nummer + nummer2;
+//
+//        System.out.println(uitkomst);
+
+        /*
+       String tekst = "Hoi ik ben Mitch";
+       double decimaal = 1.0;
+       float floating = 1.0F;
+         */
+
+        // int - 1
+        // naam - 2
+        // = - 3
+        // Wat gaat het worden - 4
+
+                /*
+            > = groter dan
+            < = kleiner dan
+            >= Groter of gelijkaan
+            <= Kleiner en gelijk aan
+            == Gelijk aan
+            != Niet gelijk aan
+
+            || = of
+            && = En
+
+
+         */
+    /*
+          Opdracht 1 :
+
+          Opdracht 2 :
+     */
+
+                int nummer1 = 1;
+                int nummer2 = 3;
+                int nummer = nummer1 + 2;
+
+                test3(3, 5);
+//        test3(4, 5);
+//        test3(5, 5);
+//        test3(7, 5);
+
+            }
+
+            public static void test3(int nummer1, int nummer2) {
+
+                if(nummer1 == 3 || nummer2 == 3 ) {
+                    System.out.println("Test klopt");
+                } else {
+                    System.out.println("Test klopt niet");
+                }
+            }
+        }
+}
 }
