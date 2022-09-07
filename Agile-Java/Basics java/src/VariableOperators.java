0a16a9ca1250ac7d54be27993ed93b11bddca238
VP-83 <111268624+VP-83@users.noreply.github.com>
2022-09-07 15:18:41 -0100

    msg

--- Agile-Java/Basics java/src/VariableOperators.java
+++ Agile-Java/Basics java/src/VariableOperators.java
@@ -20,13 +20,14 @@ public class VariableOperators {
         Opdracht 2 : Probeer een String en een int bij elkaar op te tellen
         en print het resultaat in je console.
     */
-// uitvoering opdrachten 2,3,4,5
+// uitvoering opdrachten
         String namepets = "Ben has a total of ..pets: ";
         int cats = 5;
         int dogs = 4;
         int pets = cats + dogs;
+   // opdracht 2
         System.out.println(namepets+ pets);
-
+// opdracht 3,4,5
         String namemore = "He gets more off the streets and now has ..cats: ";
         int NumberOfCats = 10;
         int newcats = NumberOfCats + 5;
@@ -34,6 +35,24 @@ public class VariableOperators {
 
         String namemax = "He can only keep 4% of his cats so now he has ";
         System.out.println(namemax+ newcats % 4);
+// Opdacht 6
+        System.out.println(cats > dogs);
+        System.out.println(cats < dogs);
+        System.out.println(cats >= dogs);
+        System.out.println(cats <= dogs);
+        System.out.println(cats == dogs);
+        System.out.println(cats != dogs);
+
+/*  Opdracht 7
+    1. Wat is een voordeel van Strict Typing?
+    - Static typing makes it easier to work with relational databases and other systems
+    which also rely on static types
+    — It helps you catch type mismatches sooner at compile-time.
+    It can help reduce the likelihood of some kinds of errors.
+
+    2. Wat is een voordeel van Weak Typing?
+    - Het kost minder moeite om een code te schrijven
+ */
 
     }
 }
