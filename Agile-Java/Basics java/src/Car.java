031e3ccfdca06c1ed7a19a51e955d8e4f2c13c4f
VP-83 <111268624+VP-83@users.noreply.github.com>
2022-09-16 08:21:05 -0100

    Fix

---
+++ Agile-Java/Basics java/src/Car.java
@@ -0,0 +1,127 @@
+public class Car {
+    public static void main(String[] args) {
+        System.out.println("dit is de main object van car");
+
+    }
+}
+/*
+public class Car {
+    public String color;
+
+}
+
+Voorbeeld van Bonieta:
+public class Car {
+    final String color;
+public Car (String color);
+
+    {
+        this.color = color;
+
+        public static void myMethod (String () args) {
+
+
+        System.out.println(Color);
+    }
+
+
+    }
+
+    Voorbeeld van Nathan CAR:
+    public class Car {
+     String brand;                // when static is added, all brand and color
+                                         // outputs set to the last added values. memory adressess do still differ
+     String color;
+
+    String name;
+
+ public  Car(String b, String c) {
+//        this.brand = b;
+//        this.color = c;
+
+     this.brand = b;
+     if (brand == null) {
+         brand = "a placeholder";}
+
+     this.color = c;
+     if (color == null) {
+         color = "placeholder";}
+
+     this.name = null;
+     if (name == null) {
+         name = "a placeholder";}
+    }
+
+   public Car(String b, String c, String n) {
+        this.brand = b;
+       if (brand == null) {
+           brand = "a placeholder";}
+
+        this.color = c;
+       if (color == null) {
+           color = "placeholder";}
+
+        this.name = n;
+       if (name == null) {
+           name = "a placeholder";}
+ }
+
+//    public Car() {
+//        this.brand = "placeholder";
+//        this.color = "placeholder";
+//        this.name ="placeholder";
+    public Car() {
+    this.brand = null;
+        if (brand == null) {
+            brand = "a placeholder";}
+     this.color = null;
+        if (color == null) {
+            color = "placeholder";}
+      this.name = null;
+        if (name == null) {
+            name = "a placeholder";}
+    }
+    void  Display(){
+         System.out.println(      //display is een method bestaande uit sout en strings die icm een car instance called kan worden om "brand= b(brand), color= c(color)" als output te krijgen (zie main)
+                            "brand= "
+                         + brand
+                         + ", "
+                         + "color= "
+                         + color
+                         + ", "
+                         + "name= "
+                         + name);
+     }
+    void printColor(){
+        System.out.println(color);                  //method voor OPDR3
+    }
+
+public String toString() {
+     return  "This is a "
+                        +color
+                        +" colored car."
+                        + " The brand is "
+                        + brand
+                        + ", "
+                        + "it's name is "
+                        + name
+                        + ".";}
+}
+
+
+
+//    public static void main(String[] args) {
+//      Car c1 = new Car("Volvo","Forest Green", " ");
+//      Car c2 = new Car("Lada","Cardboard Brown", "Apyr");
+//
+//
+//
+//        c1.display();                           // brand= Volvo, color= Forest Green
+//        c2.display();                           // brand= Lada, color= Cardboard Brown
+//        c2.namedDisplay();
+        //System.out.println(c1);     // Numan.Car@5b480cf9
+
+       // c1.printColor();                    // OPDR 3 method call, output = forest green
+
+
+ */
---
+++ Agile-Java/Basics java/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("dit is de main van car");
+    }
+/* Voorbeeld Nathan MAIN:
+    public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car("Volvo", "Forest Green");
+        Car car2 = new Car("Lada", "Cardboard Brown", "Apyr (друг)");
+        Car car3 = new Car();
+        Car car4 = new Car();
+
+            car3.brand="Kamaz";                           //adding or adjusting values is possible
+            car3.color="slim, possibly shady";
+            car3.name="Bintliy";
+
+        System.out.println(car1);                      //toString has been adjusted in class Car to give a custom return instead of hashcode
+        System.out.println(car2);
+        System.out.println(car3);
+        System.out.println(car4);
+
+        car1.Display();                                         // brand= Volvo, color= Forest Green
+        car2.Display();                                          // brand= Lada, color= Cardboard Brown, name= Apyr
+        car3.Display();                                          //brand= Kamaz, color= Questionable, name= Bintliy
+        car4.Display();
+
+        car1.printColor();
+        car2.printColor();
+        car3.printColor();
+        car4.printColor();
+    }
+}
+
+ */
+}
